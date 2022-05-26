a6038de8176d5a159c78642c37e68953a8c499bb
TudiSP <tudor.hermenean@gmail.com>
2022-05-26 20:44:28 +0300

    added new parameter to kernel

--- tema3/skl/sol/main.cu
+++ tema3/skl/sol/main.cu
@@ -9,11 +9,10 @@ using namespace std;
 // aprox 1 mil
 #define MSIZE (1 << 16)
 
-__global__ void pop_in_range(float *lat, float *lon, int *pop, int kmrange,
-                             int n) {
+__global__ void pop_in_range(float *lat, float *lon, int *pop, int *accpop,
+                             int kmrange, int n) {
   unsigned int i = threadIdx.x + blockDim.x * blockIdx.x;
 
-  int accpop = 0;
   for (int j = 0; j < n; j++) {
     float phi1 = (90.f - lat[i]) * DEGREE_TO_RADIANS;
     float phi2 = (90.f - lat[j]) * DEGREE_TO_RADIANS;
@@ -29,7 +28,7 @@ __global__ void pop_in_range(float *lat, float *lon, int *pop, int kmrange,
       cs = -1;
     }
     if ((6371.f * acos(cs)) <= kmrange) {
-      accpop += pop[j];
+      accpop[i] += pop[j];
     }
   }
 }
@@ -54,9 +53,11 @@ int main(int argc, char **argv) {
       i++;
     }
     int nrOfElements = i - 1;
+    int *accpop = (int *)malloc(nrOfElements * sizeof(int));
+
     float *device_lat = 0;
     float *device_lon = 0;
-    int *device_pop = 0;
+    int *device_pop = 0, *device_accpop = 0;
 
     // Allocating the device's array; notice that we use a special
     // function named cudaMalloc that takes the reference of the
@@ -64,10 +65,13 @@ int main(int argc, char **argv) {
     cudaMalloc((void **)&device_lat, nrOfElements * sizeof(float));
     cudaMalloc((void **)&device_lon, nrOfElements * sizeof(float));
     cudaMalloc((void **)&device_pop, nrOfElements * sizeof(int));
+    cudaMalloc((void **)&device_accpop, nrOfElements * sizeof(int));
 
     cudaMemcpy(device_lat, lat, nrOfElements * sizeof(float),
                cudaMemcpyHostToDevice);
     cudaMemcpy(device_lon, lon, nrOfElements * sizeof(float),
+               cudaMemcpyHostToDevice);
+    cudaMemcpy(device_pop, pop, nrOfElements * sizeof(float),
                cudaMemcpyHostToDevice);
 
     const size_t block_size = 256;
@@ -78,23 +82,26 @@ int main(int argc, char **argv) {
 
     // Launch the kernel
     pop_in_range<<<blocks_no, block_size>>>(device_lat, device_lon, device_pop,
-                                            kmRange, nrOfElements);
+                                            device_accpop, kmRange,
+                                            nrOfElements);
 
     // Copy the result back to the host memory space
-    cudaMemcpy(pop, device_pop, nrOfElements * sizeof(int),
+    cudaMemcpy(accpop, device_accpop, nrOfElements * sizeof(int),
                cudaMemcpyDeviceToHost);
 
     for (i = 0; i < nrOfElements; i++) {
-      ofs << pop[i] << endl;
+      ofs << accpop[i] << endl;
     }
 
     // Deallocate memory
     free(lat);
     free(lon);
     free(pop);
+    free(accpop);
     cudaFree(device_lat);
     cudaFree(device_lon);
     cudaFree(device_pop);
+    cudaFree(device_accpop);
 
     ifs.close();
     ofs.close();
